4a97856b385c35f5c9bb75b9e68d524c456549cf
twicks95 <teguh.wck95@gmail.com>
2021-06-04 22:10:53 +0700

    Set default phosphor icon style with react context

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 import "../styles/globals.css";
+import { IconContext } from "phosphor-react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          contenct="width=device-width, initial-scale=1"
-        ></meta>
-      </Head>
-      <Component {...pageProps} />
+      <IconContext.Provider
+        value={{
+          color: "rgba(58, 61, 66, 0.8)",
+          size: "1.2em",
+          weight: "bold",
+          mirrored: false,
+        }}
+      >
+        <Head>
+          <meta
+            name="viewport"
+            contenct="width=device-width, initial-scale=1"
+          ></meta>
+        </Head>
+        <Component {...pageProps} />
+      </IconContext.Provider>
     </>
   );
 }
