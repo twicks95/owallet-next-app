49001f7d2dcd769a062ed6286dc1b835f2a61007
twicks95 <teguh.wck95@gmail.com>
2021-06-08 19:40:05 +0700

    Update axios utility

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosApiInstances = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/",
+  baseURL: "http://localhost:3004/api/v1/",
 });
 
 export default axiosApiInstances;
