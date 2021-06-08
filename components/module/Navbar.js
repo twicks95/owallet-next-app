4da9bd30f0865b413eebb2c88c5c4d0de42a8338
twicks95 <teguh.wck95@gmail.com>
2021-06-08 19:34:37 +0700

    Update navbar, pagenav, and layout component

--- components/module/Navbar.js
+++ components/module/Navbar.js
@@ -1,31 +1,45 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Cookie from "js-cookie";
 import styles from "../../styles/Navbar.module.css";
 import { BellSimple } from "phosphor-react";
 
-export default function Navbar() {
-  const router = useRouter();
+// export async function getServerSideProps(context) {
+//   await authorizationPage(context);
+//   return { props: {} };
+// }
 
-  const handleLogout = () => {
-    Cookie.remove("token");
-    Cookie.remove("userId");
-    router.push("/login");
+export default function Navbar(props) {
+  const router = useRouter();
+  const { user_id, user_image, user_name, user_phone } = props.data.data[0];
+
+  const handleProfile = (id) => {
+    router.push(`/profile/${id}`);
   };
+
   return (
     <div
       className={`d-flex align-items-center justify-content-between ${styles.navbarContainer}`}
     >
       <Link href="/">Owallet</Link>
       <div className={`d-flex align-items-center ${styles.userNotif}`}>
-        <div className={`${styles.avatar}`}>
-          <img src="/user.png" alt="avatar" />
+        <div
+          className={`${styles.avatar}`}
+          onClick={() => handleProfile(user_id)}
+        >
+          <img
+            src={
+              user_image
+                ? `http://localhost:3004/api/${user_image}`
+                : "/default-img-placeholder.png"
+            }
+            alt="avatar"
+          />
         </div>
         <div
           className={`d-flex flex-column justify-content-center ${styles.userInfo}`}
         >
-          <p>Robert Chandler</p>
-          <span>+62 8139 3877 7946</span>
+          <p>{user_name}</p>
+          <span>+62 {user_phone}</span>
         </div>
         <BellSimple size={24} className={styles.bell} />
       </div>
--- components/module/PageNav.js
+++ components/module/PageNav.js
@@ -1,30 +1,132 @@
-import React from "react";
-import { ArrowUp, Plus, SignOut, SquaresFour, User } from "phosphor-react";
+import Cookie from "js-cookie";
+import { useRouter } from "next/router";
+import {
+  ArrowUp,
+  IconContext,
+  Plus,
+  SignOut,
+  SquaresFour,
+  User,
+} from "phosphor-react";
 import styles from "../../styles/PageNav.module.css";
 
-export default function PageNav() {
+export default function PageNav(props) {
+  const router = useRouter();
+  const { page } = props;
+
+  const handleLogout = () => {
+    Cookie.remove("token");
+    Cookie.remove("userId");
+    router.push("/login");
+  };
+
+  const handleClick = (e) => {
+    const target = e.target.innerText.toLowerCase();
+    switch (target) {
+      case "dashboard":
+        router.push("/");
+        break;
+      case "transfer":
+        router.push("/transfer");
+        break;
+      case "top up":
+        router.push("/topup");
+        break;
+      case "profile":
+        router.push("/profile/2");
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={`text-start ${styles.navContainer}`}>
       <ul>
-        <li>
-          <SquaresFour className="me-3" />
-          Dashboard
+        <li
+          className={page === "dashboard" ? styles.active : ""}
+          name="test"
+          onClick={(e) => handleClick(e)}
+        >
+          {page === "dashboard" ? (
+            <IconContext.Provider
+              value={{
+                color: "#6379f4",
+                size: "1.2em",
+                weight: "bold",
+                mirrored: false,
+              }}
+            >
+              <SquaresFour className="me-3" />
+            </IconContext.Provider>
+          ) : (
+            <SquaresFour className="me-3" />
+          )}
+          <span>Dashboard</span>
         </li>
-        <li>
-          <ArrowUp className="me-3" />
-          Transfer
+        <li
+          className={page === "transfer" ? styles.active : ""}
+          onClick={(e) => handleClick(e)}
+        >
+          {page === "transfer" ? (
+            <IconContext.Provider
+              value={{
+                color: "#6379f4",
+                size: "1.2em",
+                weight: "bold",
+                mirrored: false,
+              }}
+            >
+              <ArrowUp className="me-3" />
+            </IconContext.Provider>
+          ) : (
+            <ArrowUp className="me-3" />
+          )}
+          <span>Transfer</span>
         </li>
-        <li>
-          <Plus className="me-3" />
-          Top Up
+        <li
+          className={page === "topup" ? styles.active : ""}
+          onClick={(e) => handleClick(e)}
+        >
+          {page === "topup" ? (
+            <IconContext.Provider
+              value={{
+                color: "#6379f4",
+                size: "1.2em",
+                weight: "bold",
+                mirrored: false,
+              }}
+            >
+              <Plus className="me-3" />
+            </IconContext.Provider>
+          ) : (
+            <Plus className="me-3" />
+          )}
+          <span>Top Up</span>
         </li>
-        <li>
-          <User className="me-3" />
-          Profile
+        <li
+          className={page === "profile" ? styles.active : ""}
+          onClick={(e) => handleClick(e)}
+        >
+          {page === "profile" ? (
+            <IconContext.Provider
+              value={{
+                color: "#6379f4",
+                size: "1.2em",
+                weight: "bold",
+                mirrored: false,
+              }}
+            >
+              <User className="me-3" />
+            </IconContext.Provider>
+          ) : (
+            <User className="me-3" />
+          )}
+          <span>Profile</span>
         </li>
-        <li>
+        <li onClick={handleLogout}>
           <SignOut className="me-3" />
-          Logout
+          <span>Logout</span>
         </li>
       </ul>
     </div>
