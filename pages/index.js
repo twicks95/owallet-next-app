1c6a685a15d9afe5dda25ff12d10ef746ca065db
twicks95 <teguh.wck95@gmail.com>
2021-06-14 06:48:32 +0700

    Create landing page

--- pages/index.js
+++ pages/index.js
@@ -1,138 +1,66 @@
-import { useState } from "react";
-import Layout from "../components/Layout";
-import styles from "../styles/Dashboard.module.css";
-import Cookies from "next-cookies";
-import Navbar from "../components/module/Navbar";
-import Footer from "../components/module/Footer";
-import PageNav from "../components/module/PageNav";
-import axiosApiInstances from "../utils/axios";
-import { authorizationPage } from "../middleware/authorizationPage";
-import { Button } from "react-bootstrap";
-import { ArrowUp, IconContext, Plus } from "phosphor-react";
+import Layout from "components/Layout";
+import styles from "styles/Landing.module.css";
 import Link from "next/link";
+import { Button } from "react-bootstrap";
+import { unauthorizationPage } from "middleware/authorizationPage";
+import { useRouter } from "next/router";
 
-export async function getServerSideProps(context) {
-  const data = await authorizationPage(context);
-  const res = await axiosApiInstances
-    .get(`user/${data.userId}`)
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      console.log(err.response);
-      return [];
-    });
-  return {
-    props: { data: res },
+export default function Landing() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/login");
   };
-}
 
-export default function Home(props) {
-  // const [users, setUsers] = useState(props.users);
-  const { balance, user_phone } = props.data.data[0];
+  const handleRegister = () => {
+    router.push("/register");
+  };
 
   return (
-    <Layout title="Dashboard">
-      <Navbar data={props.data} />
-      <div className={`container-fluid ${styles.outerContainer}`}>
-        <div className="row row-cols-1 row-cols-md-2 h-100">
-          <div className={`col-md-3 ${styles.navSide}`}>
-            <PageNav data={props.data} page="dashboard" />
-          </div>
-          <div className={`col-md-9 ${styles.profileSide}`}>
-            <div className={`row row-cols-1 gy-3 ${styles.dashboardContainer}`}>
-              <IconContext.Provider
-                value={{
-                  color: "#B5B0ED",
-                  size: "1.2em",
-                  weight: "bold",
-                  mirrored: false,
-                }}
-              >
-                <div
-                  className={`col-12 d-flex align-items-center justify-content-between ${styles.head}`}
-                >
-                  <div className="text-start">
-                    <h2>Balance</h2>
-                    <p>Rp{balance}</p>
-                    <span>+62 {user_phone}</span>
-                  </div>
-                  <div className="d-flex flex-column gap-3">
-                    <Button
-                      variant="primary"
-                      className={`d-flex align-items-center justify-content-evenly`}
-                    >
-                      <ArrowUp className="me-2" />
-                      Transfer
-                    </Button>
-                    <Button
-                      variant="primary"
-                      className={`d-flex align-items-center justify-content-evenly`}
-                    >
-                      <Plus className="me-2" />
-                      Top Up
-                    </Button>
-                  </div>
-                </div>
-              </IconContext.Provider>
-              <div className={`col-12 p-0 ${styles.main}`}>
-                <div className={`row row-cols-2`}>
-                  <div className={`col-6 pe-0`}>
-                    <div className={`${styles.stat}`}>Graph</div>
-                  </div>
-                  <div className={`col-6`}>
-                    <div className={`${styles.history}`}>
-                      <div
-                        className={`d-flex align-items-center justify-content-between ${styles.historyHead}`}
-                      >
-                        <h2>Transaction History</h2>
-                        <Link href="/">See all</Link>
-                      </div>
-                      <div
-                        className={`d-flex align-items-center justify-content-between ${styles.list}`}
-                      >
-                        <div className="d-flex text-start">
-                          <img src="/user2.png" className="me-2" />
-                          <div className="d-flex flex-column justify-content-center">
-                            <h5 className="m-0">Samuel Umtiti</h5>
-                            <span>Transfer</span>
-                          </div>
-                        </div>
-                        <p className={`m-0 ${styles.transfer}`}>+Rp50.000</p>
-                      </div>
-                      <div
-                        className={`d-flex align-items-center justify-content-between ${styles.list}`}
-                      >
-                        <div className="d-flex text-start">
-                          <img src="/user2.png" className="me-2" />
-                          <div className="d-flex flex-column justify-content-center">
-                            <h5 className="m-0">Samuel Umtiti</h5>
-                            <span>Transfer</span>
-                          </div>
-                        </div>
-                        <p className={`m-0 ${styles.transfer}`}>+Rp50.000</p>
-                      </div>
-                      <div
-                        className={`d-flex align-items-center justify-content-between ${styles.list}`}
-                      >
-                        <div className="d-flex text-start">
-                          <img src="/user2.png" className="me-2" />
-                          <div className="d-flex flex-column justify-content-center">
-                            <h5 className="m-0">Samuel Umtiti</h5>
-                            <span>Transfer</span>
-                          </div>
-                        </div>
-                        <p className={`m-0 ${styles.transfer}`}>+Rp50.000</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+    <Layout title="Landing Page">
+      <div className={styles.landingNav}>
+        <Link href="/">Owallet</Link>
+        <div>
+          <Button
+            variant="primary"
+            style={{ border: "1.5px solid #ffffff" }}
+            onClick={handleLogin}
+          >
+            Sign In
+          </Button>
+          <Button
+            variant="light"
+            className={styles.btnLight}
+            onClick={handleRegister}
+          >
+            Sign Up
+          </Button>
         </div>
       </div>
-      <Footer />
+      <div className={`${styles.hero}`}>
+        <div>
+          <h1>Awesome App For Saving Time.</h1>
+          <p>
+            We bring you a mobile app for banking problems that oftenly wasting
+            much of your times.
+          </p>
+          <Button
+            variant="light"
+            className={styles.btnLight}
+            onClick={handleRegister}
+          >
+            Tyr It Free
+          </Button>
+        </div>
+        <img src="/phone2.png" alt="mockup" />
+      </div>
     </Layout>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  await unauthorizationPage(context);
+  return {
+    props: {},
+  };
+};
