8d47aa9dbfd2d2be4793ed010bdbc9c1d29de50f
twicks95 <teguh.wck95@gmail.com>
2021-07-14 15:05:43 +0700

    Update: axios utility

--- utils/axios.js
+++ utils/axios.js
@@ -1,43 +1,44 @@
 import axios from "axios";
+import Cookie from "js-cookie";
 
-const axiosApiInstances = axios.create({
+const axiosApiIntances = axios.create({
   baseURL: process.env.API_BASE_URL,
 });
 
-// Add a request interceptor
-// axiosApiInstances.interceptors.request.use(
-//   function (config) {
-//     // Do something before request is sent
-//     // set bagian headers
-//     config.headers = {
-//       Authorization: `Bearer ${localStorage.getItem("token")}`,
-//     };
+if (Cookie.get("token")) {
+  axiosApiIntances.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      config.headers = {
+        Authorization: `Bearer ${Cookie.get("token")}`,
+      };
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+}
 
-//     return config;
-//   },
-//   function (error) {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   }
-// );
+// Add a response interceptor
+axiosApiIntances.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 403) {
+      alert("Please Login !");
+      Cookie.remove("token");
+      Cookie.remove("user");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
-// // Add a response interceptor
-// axiosApiInstances.interceptors.response.use(
-//   function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response;
-//   },
-//   function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     if (error.response.status === 403) {
-//       alert("Please login first!");
-//       localStorage.clear();
-//       window.location.href = "/sign-in";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-export default axiosApiInstances;
+export default axiosApiIntances;
