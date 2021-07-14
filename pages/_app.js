d725143c0d745467ad8f9c81c5d4f5030fb5d65d
twicks95 <teguh.wck95@gmail.com>
2021-07-14 15:11:56 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,36 @@
 import Head from "next/head";
-import { IconContext } from "phosphor-react";
+import { Provider } from "react-redux";
+import { useStore } from "redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { clientStore, persistor } from "redux/store";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
+import { IconContext } from "phosphor-react";
 
 function MyApp({ Component, pageProps }) {
+  const store = useStore(pageProps.initialReduxState);
+
   return (
-    <>
-      <IconContext.Provider
-        value={{
-          color: "rgba(58, 61, 66, 0.8)",
-          size: "1.2em",
-          weight: "bold",
-          mirrored: false,
-        }}
-      >
-        <Head>
-          <meta
-            name="viewport"
-            contenct="width=device-width, initial-scale=1"
-          ></meta>
-        </Head>
-        <Component {...pageProps} />
-      </IconContext.Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <IconContext.Provider
+          value={{
+            color: "rgba(58, 61, 66, 0.8)",
+            size: "1.2em",
+            weight: "bold",
+            mirrored: false,
+          }}
+        >
+          <Head>
+            <meta
+              name="viewport"
+              contenct="width=device-width, initial-scale=1"
+            ></meta>
+          </Head>
+          <Component {...pageProps} />
+        </IconContext.Provider>
+      </PersistGate>
+    </Provider>
   );
 }
 
